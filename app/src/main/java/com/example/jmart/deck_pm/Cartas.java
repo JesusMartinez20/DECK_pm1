13ee6950b6c931d17943f53ad8a96a8083686238
TheRaven0 <33192268+TheRaven0@users.noreply.github.com>
2018-05-10 18:56:22 -0500

    Agregado el POJO "Cartas" al projecto

---
+++ app/src/main/java/com/example/jmart/deck_pm/Cartas.java
@@ -0,0 +1,9 @@
+package com.example.jmart.deck_pm;
+
+public class Cartas {
+    public String nombre;
+    public String descripcion;
+    public String puntos;
+    public String imagen;
+    public int id;
+}
